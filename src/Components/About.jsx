cd2808048349151ad0b5b86ed5ff5417497f9790
Akshat Sengar <100029176+theakshatsengar@users.noreply.github.com>
2024-03-17 13:04:49 +0000

    Edited Forked Repo

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,17 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Electronics student studying at Gurukul Kangri University. I enjoy creating unique and simplistic webpages in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "Web Devlopement",
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "React",
+  "Javascript",
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my devlopement experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
